2015d942fae1ff13cab4e011e6bb8740d00cffe2
Ahmed-Khaled-Abdelmaksod <ak4902280@gmail.com>
2026-03-13 02:37:04 +0200

    feat: add Authentication filter.

---
+++ e-commerce/src/main/java/gov/iti/jets/ecommerce/filters/AuthFilter.java
@@ -0,0 +1,24 @@
+package gov.iti.jets.ecommerce.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/sweets.jsp"}) // add uri of the guarded areas
+public class AuthFilter extends HttpFilter {
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = request.getSession(false);
+        boolean isLoggedIn = (session != null && "true".equals(session.getAttribute("loggedIn")));
+        if (isLoggedIn) {
+            // User is valid!
+            chain.doFilter(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/login");
+        }
+    }
+}
