bb75b0d981151235f57f69e9f5ea80f83045da7b
Ahmed-Khaled-Abdelmaksod <ak4902280@gmail.com>
2026-03-13 14:39:52 +0200

    feat: add Encryption Utility.

--- e-commerce/src/main/java/gov/iti/jets/ecommerce/service/AuthService.java
+++ e-commerce/src/main/java/gov/iti/jets/ecommerce/service/AuthService.java
@@ -4,6 +4,7 @@ import gov.iti.jets.ecommerce.beans.UserBean;
 import gov.iti.jets.ecommerce.config.DataSourceConfig;
 import gov.iti.jets.ecommerce.dao.impl.UserDaoImpl;
 import gov.iti.jets.ecommerce.entity.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.Optional;
 
@@ -20,16 +21,18 @@ public class AuthService {
         Optional<User> user = userDao.findByEmail(email);
         if(user.isPresent()) {
             User u = user.get();
-            UserBean userBean = new UserBean();
-            userBean.setUserId(u.getUserId());
-            userBean.setEmail(u.getEmail());
-            userBean.setRole(u.getRole());
-            userBean.setAddress(u.getAddress());
-            userBean.setCreditBalance(u.getCreditBalance());
-            userBean.setBirthday(u.getBirthday());
-            userBean.setFullName(u.getFullName());
-            userBean.setPhone(u.getPhone());
-            return Optional.of(userBean);
+            if(BCrypt.checkpw(password,u.getPasswordHash())) {
+                UserBean userBean = new UserBean();
+                userBean.setUserId(u.getUserId());
+                userBean.setEmail(u.getEmail());
+                userBean.setRole(u.getRole());
+                userBean.setAddress(u.getAddress());
+                userBean.setCreditBalance(u.getCreditBalance());
+                userBean.setBirthday(u.getBirthday());
+                userBean.setFullName(u.getFullName());
+                userBean.setPhone(u.getPhone());
+                return Optional.of(userBean);
+            }
         }
         return Optional.empty();
     }
