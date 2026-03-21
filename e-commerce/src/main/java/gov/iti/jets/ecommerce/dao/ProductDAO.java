3c374c5e46cde17c90fa1363cdb274775f0d7e63
Ahmed-Khaled-Abdelmaksod <ak4902280@gmail.com>
2026-03-21 15:07:32 +0200

    feat: implement findByCategoryId and searchByName in ProductDaoImpl class.

--- e-commerce/src/main/java/gov/iti/jets/ecommerce/dao/ProductDAO.java
+++ e-commerce/src/main/java/gov/iti/jets/ecommerce/dao/ProductDAO.java
@@ -1,6 +1,8 @@
 package gov.iti.jets.ecommerce.dao;
 
 import gov.iti.jets.ecommerce.entity.Product;
+import jakarta.persistence.EntityManager;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -12,9 +14,9 @@ public interface ProductDAO {
 
     Optional<Product> findById(int id);
 
-    List<Product> findByCategoryId(int categoryId);
+    List<Product> findByCategoryId(EntityManager em , int categoryId);
 
-    List<Product> searchByName(String name);
+    List<Product> searchByName(EntityManager em,String name);
 
     boolean update(Product product);
 
