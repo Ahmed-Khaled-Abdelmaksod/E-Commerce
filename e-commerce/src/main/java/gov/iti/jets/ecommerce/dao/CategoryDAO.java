ebef68309942640563c174a468a00991784d2607
MAshour2025 <mohammedashour850@gmail.com>
2026-03-23 23:14:36 +0200

    version 2

--- e-commerce/src/main/java/gov/iti/jets/ecommerce/dao/CategoryDAO.java
+++ e-commerce/src/main/java/gov/iti/jets/ecommerce/dao/CategoryDAO.java
@@ -2,20 +2,21 @@ package gov.iti.jets.ecommerce.dao;
 
 import gov.iti.jets.ecommerce.entity.Category;
 import jakarta.persistence.EntityManager;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryDAO {
 
-    Category insert(EntityManager em, Category category);
+    Category insert(EntityManager em,Category category);
 
     List<Category> findAll(EntityManager em);
 
-    Optional<Category> findById(EntityManager em, int id);
+    Optional<Category> findById(EntityManager em,int id);
 
-    boolean update(EntityManager em, Category category);
+    boolean update(EntityManager em,Category category);
 
-    boolean delete(EntityManager em, int id);
+    boolean delete(EntityManager em,int id);
 
-    boolean existsByName(EntityManager em, String name);
+    boolean existsByName(EntityManager em,String name);
 }
